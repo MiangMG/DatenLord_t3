42e5b4cd556df4f60bf1469a91e2a30fd49d985d
MG <106969524+MiangMG@users.noreply.github.com>
2022-06-10 18:01:33 +0800

    假定valid和ready信号都不满足时序要求，都需要用寄存器打一拍，实现该总线握手场景
    场景3_delay_all
    中间加了两组寄存器，a和b，a和b轮流接收新数据与读出数据。
    复位后，两寄存器均为空，默认先存寄存器a，再存寄存器b，两个都存满的话寄存器ready拉低。不再与主机建立握手。
    从机读取时，默认先读取a。只要a和b任意一个有值，寄存器valid就拉高，表示可以与目的端建立握手。
    总的行为是：主机在与 读空信号进行握手，读空信号本质就相当于从机ready的延拍；从机在于 缓存有效信号进行握手，缓存有效本质就是 主机valid的延拍。

---
+++ tb_Handshakes_All.v
@@ -0,0 +1,77 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2014-2022 All rights reserved
+// -----------------------------------------------------------------------------
+// Author : 1598491517@qq.com
+// File   : tb_Handshakes_All.v
+// Create : 2022-06-09 16:34:58
+// Revise : 2022-06-10 15:39:26
+// Editor : sublime text3, tab size (4)
+// -----------------------------------------------------------------------------
+`timescale 1 ns / 1 ps
+
+module tb_Handshakes_All();
+reg clk;
+reg rst_n;
+reg up_valid;
+reg [7:0] up_data;
+reg down_ready;
+wire down_valid;
+wire [7:0]down_data;
+wire up_ready;
+
+initial begin
+	clk = 1'b0;
+	rst_n = 1'b0;
+	up_valid = 1'b0;
+	up_data = 'd0;
+	down_ready = 1'b0;
+	#35
+	rst_n = 1'b1;
+	down_ready = 1'b0;
+	up_valid = 1'b1;
+	#40
+	down_ready = 1'b1;
+	#20
+	down_ready = 1'b0;
+	#60
+	down_ready = 1'b1;
+	#40
+	down_ready = 1'b0;
+	#60
+	down_ready = 1'b1;
+	#120
+	down_ready = 1'b0;
+	up_valid = 1'b0;
+	#20
+	down_ready = 1'b1;	
+	#40
+	down_ready = 1'b0;
+end
+
+always @(posedge clk)begin
+	if(!rst_n)begin
+		up_data <= 'd0;
+	end
+	else if(up_ready && up_valid)begin
+		up_data <= {$random}%256;
+	end
+end
+
+
+always #10 clk = ~clk;
+
+	Handshakes_All #(
+			.WORD_WIDTH(8)
+		) inst_Handshakes_All (
+			.clk        (clk),
+			.rst_n      (rst_n),
+			.up_valid   (up_valid),
+			.up_data    (up_data),
+			.down_ready (down_ready),
+			.down_valid (down_valid),
+			.down_data  (down_data),
+			.up_ready   (up_ready)
+		);
+
+
+endmodule
